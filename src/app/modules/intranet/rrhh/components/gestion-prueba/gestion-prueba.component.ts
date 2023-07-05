dbda81b93eefe78faf8d345bbcdb4dabbd7d5342
Giancarlos Gutiérrez Cervantes <ggutierrez@canvia.com>
2023-07-04 21:41:30 -0500

    reactive forms

--- src/app/modules/intranet/rrhh/components/gestion-prueba/gestion-prueba.component.ts
+++ src/app/modules/intranet/rrhh/components/gestion-prueba/gestion-prueba.component.ts
@@ -4,6 +4,8 @@ import {
   GestionPruebaHijoModalComponent
 } from "./components/gestion-prueba-hijo-modal/gestion-prueba-hijo-modal.component";
 import {GestionPruebaService} from "../../../../../services/gestion-prueba.service";
+import {Router} from "@angular/router";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-gestion-prueba',
@@ -29,13 +31,44 @@ export class GestionPruebaComponent implements OnInit {
   textoUpperCasePipe: string = "pipes en angular";
   fecha: Date = new Date();
 
+  //D32
+  /* Formularios NgModel */
+  nombreUsuarioNgModel: string = '';
+
+  usuario: any = {
+    "id": "1",
+    "nombre": "Luisin Maza"
+  };
+
+  //D33
+  /* Formularios Reactivos FormGroup */
+  formaPersona: FormGroup;
+
+  formaPersonaBuilder: FormGroup;
+
   constructor(private modalService: NgbModal,
               private configModal: NgbModalConfig,
-              private gestionPruebaService: GestionPruebaService) {
+              private gestionPruebaService: GestionPruebaService,
+              private router: Router,
+              public fb: FormBuilder) {
     console.log("Constructor");
     //D25
     configModal.backdrop = 'static';
     configModal.keyboard = false;
+
+    //D33
+    this.formaPersona = new FormGroup(
+      {
+        "nombre": new FormControl('', [Validators.required, Validators.maxLength(20)]),
+        "apellido": new FormControl('', [Validators.required, Validators.maxLength(20)]),
+      }
+    );
+
+    this.formaPersonaBuilder = this.fb.group(
+      {
+        "nombre": ['', Validators.required]
+      }
+    );
   }
 
   ngOnInit(): void {
@@ -48,38 +81,79 @@ export class GestionPruebaComponent implements OnInit {
 
 
   //D24
-  eventoBtnBindingEvento(){
+  eventoBtnBindingEvento() {
     alert("Evento Boton");
   }
 
-  eventoInputBindingEvento(event: any){
+  eventoInputBindingEvento(event: any) {
     console.log("Imprimir texto: ", event.target.value);
   }
 
   //D25
-  eventoBtnInputOutput(mensaje: string){
+  eventoBtnInputOutput(mensaje: string) {
     alert(mensaje);
   }
 
-  eventoBtnInputOutputModal(){
-    const modalRef = this.modalService.open(GestionPruebaHijoModalComponent, { size: 'lg' });
+  eventoBtnInputOutputModal() {
+    const modalRef = this.modalService.open(GestionPruebaHijoModalComponent, {size: 'lg'});
     modalRef.componentInstance.tituloComponenteHijo = "COMPONENTE PRUEBA HIJO (MODAL)";
     modalRef.componentInstance.nombreComponenteHijo = "Hola Mundo (Modal)";
     modalRef.componentInstance.eventoEventEmitterOutput.subscribe(
-        (result: any) => {
+      (result: any) => {
         alert(result);
       }
     );
   }
 
   //D26
-  eventoBtnProcesarServicioSinParametro(){
+  eventoBtnProcesarServicioSinParametro() {
     this.gestionPruebaService.getMiPrimerServicioSinParametro();
   }
 
-  eventoBtnProcesarServicioConParametro(){
+  eventoBtnProcesarServicioConParametro() {
     this.gestionPruebaService.getMiPrimerServicioConParametro("Hola Mundo");
   }
 
+  //D30
+  eventoBtnIrAOtraVista() {
+    this.router.navigate(['/rrhh/gestion-persona']);
+  }
 
+
+  //D32
+  eventoBtnValidarNgModel(formaPersonaNgModel: any) {
+
+    console.log("LEMA", formaPersonaNgModel);
+
+    if (formaPersonaNgModel.form.valid) {
+      alert('OK: ' + formaPersonaNgModel.value.nombreUsuario)
+    } else {
+      formaPersonaNgModel.form.markAllAsTouched();
+      alert('Mensaje: Completar campos obligatorios');
+    }
+
+  }
+
+  //D33
+  eventoBtnValidar(){
+
+    if (this.formaPersona.valid){
+      alert('OK: ' + this.formaPersona.get('nombre')?.value)
+    }else{
+      this.formaPersona.markAllAsTouched();
+      alert('Mensaje: Completar campos obligatorios');
+    }
+
+  }
+
+  eventoBtnValidarBuilder(){
+
+    if (this.formaPersonaBuilder.valid){
+      alert('OK: ' + this.formaPersonaBuilder.get('nombre')?.value)
+    }else{
+      this.formaPersonaBuilder.markAllAsTouched();
+      alert('Mensaje: Completar campos obligatorios');
+    }
+
+  }
 }
